ce56ce9812173aeb968eee135c9f8182bdb580eb
Joe Maloney <maloney.jg2@gmail.com>
2025-03-21 00:40:24 -0400

    define in header

--- lab 3/pinstacking.c
+++ lab 3/pinstacking.c
@@ -3,56 +3,48 @@
  *
  * Created: 3/19/2025 2:38:53 AM
  *  Author: malon
- */ 
- #include "pinstacking.h"
- #include "portAPI.h"
- #include "termcontrol.h"
- extern void UART_Init(unsigned int);
- extern void UART_Puts(const char *str);
+ */
+#include "pinstacking.h"
+
+#include "portAPI.h"
+#include "termcontrol.h"
+extern void UART_Init(unsigned int);
+extern void UART_Puts(const char *str);
 
 bool RXFlag = false;
-inline void pollForUARTReceive(){
-portWritePin(&PORTC,5,1);
-portWritePin(&PORTC,5,0);
-	if(!portReadPin(&PORTD,0)){
-		RXFlag = true;
-	}
-	
+inline void pollForUARTReceive() {
+  if (!portReadPin(&PORTD, 0)) {
+    RXFlag = true;
+  }
 }
-void nullFunction(){
-return;
+void nullFunction() { return; }
+void delayLoop(uint16_t cnt) {
+  // cnt = 6000 is ~2.6ms with -O0
+  // cnt = 3000 is ~1.3ms with -O0
+  for (uint16_t i = 0; i < cnt; i++) {
+    pollForUARTReceive();
+  }
 }
-void delayLoop(uint16_t cnt){
-//cnt = 6000 is ~2.6ms with -O0
-//cnt = 3000 is ~1.3ms with -O0
-	for(uint16_t i =0;i<cnt;i++){
-	pollForUARTReceive();
-	}
+void enableUARTCallback() {
+  UART_Init(103U);
+  delayLoop(UART_LOOP_DELAY_COUNT);
+  UART_Puts(TTCSI "u");
+  UART_Puts(TTCSI "0J");
+  delayLoop(UART_LOOP_DELAY_COUNT);
 }
-#define UART_LOOP_DELAY_COUNT 6000
-void enableUARTCallback(){
-	UART_Init(103U);
-	delayLoop(UART_LOOP_DELAY_COUNT);
-pollForUARTReceive();
-	UART_Puts(TTCSI"u");
-	UART_Puts(TTCSI"0J");
-	delayLoop(UART_LOOP_DELAY_COUNT);
-}
-void disableUARTCallback(){
-	delayLoop(UART_LOOP_DELAY_COUNT);
-	UART_Puts(TTCSI"s");
-	UCSR0B = (0<<RXEN0)|(0<<TXEN0);
-configInputPin(&PORTD,0);
-	delayLoop(UART_LOOP_DELAY_COUNT);
+void disableUARTCallback() {
+  delayLoop(UART_LOOP_DELAY_COUNT);
+  UART_Puts(TTCSI "s");
+  UCSR0B = (0 << RXEN0) | (0 << TXEN0);
+  configInputPin(&PORTD, 0);
+  delayLoop(UART_LOOP_DELAY_COUNT);
 }
 
-const PSCallbacks animateStacking= {
-	enableUARTCallback,
-disableUARTCallback};
-//pcint16
-void enableReceiveFlag(){
-configInputPin(&PORTD,0);
-pollForUARTReceive();
-return;
+const PSCallbacks animateStacking = {enableUARTCallback, disableUARTCallback};
+// pcint16
+void enableReceiveFlag() {
+  configInputPin(&PORTD, 0);
+  pollForUARTReceive();
+  return;
 }
-PSCallbacks LCDStacking = {enableReceiveFlag,nullFunction};
+PSCallbacks LCDStacking = {enableReceiveFlag, nullFunction};
--- lab 3/pinstacking.h
+++ lab 3/pinstacking.h
@@ -1,18 +1,17 @@
 /*
-* pinstacking.h
-*
-* Created: 3/18/2025 9:35:07 PM
-*  Author: Joe Maloney
-*/
-
+ * pinstacking.h
+ *
+ * Created: 3/18/2025 9:35:07 PM
+ *  Author: Joe Maloney
+ */
 
 #ifndef PINSTACKING_H_
 #define PINSTACKING_H_
-
+#define UART_LOOP_DELAY_COUNT 6000
 #include <stdbool.h>
-typedef struct PinStackingCallbackStruct{
-	void(*enable)(void);
-	void(*disable)(void);
+typedef struct PinStackingCallbackStruct {
+  void (*enable)(void);
+  void (*disable)(void);
 } PSCallbacks;
 bool RXFlag;
 void pollForUARTReceive();
