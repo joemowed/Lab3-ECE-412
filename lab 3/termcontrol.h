31080811d1b0149d564e54de48269fbc8036e95f
Joe Maloney <maloney.jg2@gmail.com>
2025-03-21 00:45:15 -0400

    file renaming

--- lab 3/termcontrol.h
+++ lab 3/termcontrol.h
@@ -2,16 +2,14 @@
  * termcontrol.h
  *
  * Created: 3/18/2025 10:46:02 PM
- *  Author: malon
- */ 
-
+ *  Author: Joe Maloney
+ */
 
 #ifndef TERMCONTROL_H_
 #define TERMCONTROL_H_
 
-
 #define TTESC "\x1B"
-#define TTCSI TTESC"["
-#define TTCR "\x0d" //terminal carriage return, sets cursor to leftmost margin
-#define TTLF "\x0a" //terminal line feed, inserts newline
-#endif /* TERMCONTROL_H_ */
+#define TTCSI TTESC "["
+#define TTCR "\x0d"  // terminal carriage return, sets cursor to leftmost margin
+#define TTLF "\x0a"  // terminal line feed, inserts newline
+#endif               /* TERMCONTROL_H_ */
