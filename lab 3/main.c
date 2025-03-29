e365521850a6cd239a62b7e304dc287de1218045
Joe Maloney <maloney.jg2@gmail.com>
2025-03-29 12:22:12 -0400

    czxcxz

--- lab 3/main.c
+++ lab 3/main.c
@@ -6,6 +6,24 @@
 //
 
 //
+
+#include "animate.h"
+#include "delay.h"
+#include "portAPI.h"
+#include "profiling.h"
+#include "pinstacking.h"
+#include "termcontrol.h"
+#include <avr/io.h>
+void UART_Init( unsigned int ubrr)
+{
+	/*Set baud rate */
+	UBRR0H = (unsigned char)(ubrr>>8);
+	UBRR0L = (unsigned char)ubrr;
+	/*	Enable receiver and transmitter */
+	UCSR0B = (1<<RXEN0)|(1<<TXEN0);
+	/* Set frame format: 8data, 2stop bit */
+	UCSR0C = (1<<USBS0)|(3<<UCSZ00);
+}
 
 const char MS1[] = "\r\nECE-412 ATMega328PB Tiny OS";
 const char MS2[] = "\r\nby Eugene Rockey Copyright 2022, All Rights Reserved";
@@ -67,122 +85,51 @@ void HELP(void)  // Display available Tiny OS Commands on Terminal
 
 void LCD(void)  // Lite LCD demo
 {
-	DATA = 0x34;  // Student Comment Here
-	LCD_Write_Command();
-	DATA = 0x08;  // Student Comment Here
-	LCD_Write_Command();
-	DATA = 0x02;  // Student Comment Here
-	LCD_Write_Command();
-	DATA = 0x06;  // Student Comment Here
-	LCD_Write_Command();
-	DATA = 0x0f;  // Student Comment Here
-	LCD_Write_Command();
-	LCD_Puts("Hello ECE412!");
-	/*
-	Re-engineer this subroutine to have the LCD endlessly scroll a marquee sign of
-	your Team's name either vertically or horizontally. Any key press should stop
-	the scrolling and return execution to the command line in Terminal. User must
-	always be able to return to command line.
-	*/
-}
-
-void ADC(void)  // Lite Demo of the Analog to Digital Converter
-{
-	volts[0x1] = '.';
-	volts[0x3] = ' ';
-	volts[0x4] = 0;
-	ADC_Get();
-	Acc = (((int)HADC) * 0x100 + (int)(LADC)) * 0xA;
-	volts[0x0] = 48 + (Acc / 0x7FE);
-	Acc = Acc % 0x7FE;
-	volts[0x2] = ((Acc * 0xA) / 0x7FE) + 48;
-	Acc = (Acc * 0xA) % 0x7FE;
-	if (Acc >= 0x3FF) volts[0x2]++;
-	if (volts[0x2] == 58) {
-		volts[0x2] = 48;
-		volts[0x0]++;
-	}
-	UART_Puts(volts);
-	UART_Puts(MS6);
-	/*
-	Re-engineer this subroutine to display temperature in degrees Fahrenheit on
-	the Terminal. The potentiometer simulates a thermistor, its varying resistance
-	simulates the varying resistance of a thermistor as it is heated and cooled.
-	See the thermistor equations in the lab 3 folder. User must always be able to
-	return to command line.
-	*/
-}
-
-void EEPROM(void) {
-	UART_Puts("\r\nEEPROM Write and Read.");
-	/*
-	Re-engineer this subroutine so that a byte of data can be written to any
-	address in EEPROM during run-time via the command line and the same byte of
-	data can be read back and verified after the power to the Xplained Mini board
-	has been cycled. Ask the user to enter a valid EEPROM address and an 8-bit
-	data value. Utilize the following two given Assembly based drivers to
-	communicate with the EEPROM. You may modify the EEPROM drivers as needed. User
-	must be able to always return to command line.
-	*/
-	UART_Puts("\r\n");
-	EEPROM_Write();
-	UART_Puts("\r\n");
-	EEPROM_Read();
-	UART_Put();
-	UART_Puts("\r\n");
-}
-
-void Command(void)  // command interpreter
-{
-	UART_Puts(MS3);
-	ASCII = '\0';
-	while (ASCII == '\0') {
-		UART_Get();
-	}
-	switch (ASCII) {
-		case 'L' | 'l':
-		LCD();
-		break;
-		case 'A' | 'a':
-		ADC();
-		break;
-		case 'E' | 'e':
-		EEPROM();
-		break;
-		default:
-		UART_Puts(MS5);
-		HELP();
-		break;
-		// Add a 'USART' command and subroutine to allow the user to reconfigure
-		// the serial port parameters during runtime. Modify baud rate, # of data
-		// bits, parity, # of stop bits.
-	}
-}
-
-#include "animate.h"
-#include "delay.h"
-#include "portAPI.h"
-#include "profiling.h"
-#include "pinstacking.h"
-#include "termcontrol.h"
-#include <avr/io.h>
-void UART_Init( unsigned int ubrr)
-{
-	/*Set baud rate */
-	UBRR0H = (unsigned char)(ubrr>>8);
-	UBRR0L = (unsigned char)ubrr;
-	/*	Enable receiver and transmitter */
-	UCSR0B = (1<<RXEN0)|(1<<TXEN0);
-	/* Set frame format: 8data, 2stop bit */
-	UCSR0C = (1<<USBS0)|(3<<UCSZ00);
-}
-
-int main(void) {
-	UCSR0B = (0<<RXEN0)|(0<<TXEN0);
-	UART_Init(103U);
-	Banner();
-	configOutputPin(&PORTC,5);
-	while (1) {
+	const char uart_config_msg[] = "UART settings will be changed.  Change terra-term settings to 9600 baud, 2 stop bits, and 8 data bits.\n Press any key when terra-term settings are established to continue.";
+	UART_Puts(uart_config_msg);
+	UART_Init(103U);// set new uart settings
+	UART_Get();// wait for user input
+	while(1)
+	{
+		if(RXFlag){
+			break;
+		}
 		animate("Arvato",&animateStacking,&LCDStacking);
 	}
 }
+
+
+	void Command(void)  // command interpreter
+	{
+		UART_Puts(MS3);
+		ASCII = '\0';
+		while (ASCII == '\0') {
+			UART_Get();
+		}
+		switch (ASCII) {
+			case 'L' | 'l':
+			LCD();
+			break;
+			case 'A' | 'a':
+			UART_Puts("This str");
+			break;
+			case 'E' | 'e':
+			break;
+			default:
+			UART_Puts(MS5);
+			HELP();
+			break;
+			// Add a 'USART' command and subroutine to allow the user to reconfigure
+			// the serial port parameters during runtime. Modify baud rate, # of data
+			// bits, parity, # of stop bits.
+		}
+	}
+
+	int main(void) {
+		UCSR0B = (0<<RXEN0)|(0<<TXEN0);
+		UART_Init(103U);
+		Banner();
+		while(1){
+			Command();
+		}
+		}
